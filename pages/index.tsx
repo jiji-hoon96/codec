cfa69eb020a73e79bb90d0d1c76eb142149ab2e2
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-24 01:23:48 +0900

    2. fetch 이용해 데이터가져와서 나열

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+interface PersonType {
+  id: string;
+  industries : [string];
+  name: string;
+  netWorth: number;
+  squareImage: string;
+}
 
 const Home: NextPage = () => {
+  const [person, setPerson]= useState<PersonType[]>([]);
+  useEffect(()=>{
+    (async ()=>{
+      const data = await(
+        await fetch(
+          "https://billions-api.nomadcoders.workers.dev/"
+        )
+      ).json();
+      setPerson(data);
+    })()
+  },[])
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">itsadf works</h1>
+    <div>
+      <div className="bg-red-500 flex-wrap justify-center items-center flex">
+        {person.map((x)=>(
+          <div className="bg-blue-500 w-28 h-28 m-1 justify-center items-center flex">
+              {x.id}
+          </div>  
+        ))}
+      </div>
     </div>
   );
 };
