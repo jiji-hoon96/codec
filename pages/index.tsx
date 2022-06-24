a0d5a6fe02b80efae1bd6a2cb9448ee9001af824
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-24 12:49:15 +0900

    4. pages/person/detail 화면 마무리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
 
 interface PersonType {
   id: string;
@@ -9,23 +10,7 @@ interface PersonType {
   netWorth: number;
   squareImage: string;
 }
-/*
-interface PersonType {
-about: [string];
-bio: [string];
-city: string;
-country:  string;
-financialAssets: [string];
-id: string;
-industries:[string];
-name: string;
-netWorth: number;
-position: 1
-squareImage:  string;
-state: string;
-thumbnail: string;
-}
-*/
+
 
 const Home: NextPage = () => {
   const [person, setPerson]= useState<PersonType[]>([]);
@@ -44,8 +29,8 @@ const Home: NextPage = () => {
     <div className="bg-slate-800">
       <div className="flex-wrap justify-center items-center flex ">
         {person.map((x)=>(
-          <Link href={`/person/${x.id}`}>
-            <div className="w-52 h-80 m-2 justify-center items-center flex-column bg-slate-700 shadow-2xl rounded hover:bg-violet-600 hover:cursor-pointer hover:scale-105 hover:ease-in-out" key={x.id}>
+          <Link href={`/person/${x.id}`}  key={uuid()}>
+            <div className="w-52 h-80 m-2 justify-center items-center flex-column bg-slate-700 shadow-2xl rounded hover:bg-violet-600 hover:cursor-pointer hover:scale-105 hover:ease-in-out" key={uuid()}>
                 <div className="w-52 h-52 bg-center bg-cover rounded"  style={{backgroundImage: `url(${x.squareImage})`}}/>
                 <div className="text-white m-1 text-center font-bold">
                   {x.id}
--- pages/person/[id].tsx
+++ pages/person/[id].tsx
@@ -1,11 +1,85 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+interface financi {
+  exchange: string;
+  ticker: string;
+  companyName: string;
+  numberOfShares: number;
+  sharePrice: number;
+  currencyCode: string;
+  exchangeRate:number;
+  interactive: boolean;
+  currentPrice:number;
+}
+interface PersonDetail {
+  id: string;
+  state: string;
+  city: string;
+  name: string;
+  country: string;
+  position: number;
+  industries: [string]
+  financialAssets: [financi],
+  thumbnail:string;
+  squareImage: string;
+  bio: [string];
+  about: [string];
+  netWorth: number;
+}
 
 const PersonDetail: NextPage = () => {
+  const {query} =useRouter();
+  const person = query.id;
+  const [detail, setDetail]= useState<PersonDetail>();
+  useEffect(()=>{
+    (async ()=>{
+      const data = await(
+        await fetch(
+          `https://billions-api.nomadcoders.workers.dev/person/${person}`
+        )
+      ).json();
+      setDetail(data);
+    })()
+  },[])
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">PersonDetail</h1>
-    </div>
+      <div className="flex-col flex pt-10 bg-slate-800 items-center">
+        <div className="bg-slate-700 px-24 mx-20 my-10 rounded-lg">
+          <div className="w-52 h-52 bg-center bg-cover rounded-lg mt-5"  style={{backgroundImage: `url(${detail?.squareImage})`}}/>
+          <div className="text-white font-bold text-3xl w-full my-2">
+            {detail?.id}
+          </div>
+          <div className="text-white w-full my-1 text-sm">
+            {`자산 순가치: $${detail?.netWorth.toLocaleString()} million`}
+          </div>
+          <div className="text-white w-full my-1 text-sm">
+            {`국가 : ${detail?.country}`}
+          </div>
+          <div className="text-white w-full my-1 text-sm">
+            {`산업: ${detail?.industries}`}
+          </div>
+          <div className="text-white w-full my-1 text-sm leading-7 mb-5">
+            {detail?.bio}
+          </div>
+        </div>      
+        <div className="bg-slate-700 px-24 mx-20 my-10 rounded-lg">
+          <div className="text-white font-bold text-3xl my-4">Financial Assets</div>
+          <div className="flex-wrap flex items-center">
+            {detail?.financialAssets.map((x)=>(
+              <div className="w-47 m-3 border p-2 rounded-xl text-white text-sm">
+                <div className="m-1">{`회사 이름 : ${x.companyName}`}</div>
+                <div className="m-1">{`주가 : ${x.sharePrice}`}</div>
+                <div className="m-1">{`증권 거래소 : ${x.exchange}`}</div>
+              </div>
+            )
+            )}
+          </div>
+          </div>
+
+      </div>
   );
 };
 
 export default PersonDetail;
+
